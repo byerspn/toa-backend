7a6e3a46301794238ad5a9a2d1b571a3ef410252
Parker Byers <parker.n.byers@gmail.com>
2021-10-22 00:15:13 -0500

    theoretically connected to local  psql db

--- toa_django/settings.py
+++ toa_django/settings.py
@@ -51,6 +51,7 @@ CORS_ALLOWED_ORIGINS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -84,8 +85,11 @@ WSGI_APPLICATION = 'toa_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'toa',
+        'USER': 'toauser',
+        'PASSWORD': 'toauserpassword',
+        'HOST': 'localhost',
     }
 }
 
