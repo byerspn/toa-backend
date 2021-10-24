623a2d31af8971b8599032547b023e67107027a2
Parker Byers <parker.n.byers@gmail.com>
2021-10-24 14:42:52 -0500

    successfully redid events seed doc to incorporate owners and entrants as user references

--- seedEvents.js
+++ seedEvents.js
@@ -22,35 +22,40 @@ const seedEvents = [
         location: 'Featheringill',
         dateandtime: 'friday night, you know the drill',
         description: 'a weekly smash tournament that is run by vandysmash, and consistently won by infamous highschooler, Snapback.',
-        entrants: 38,
+        entrants: ["6175b2ad05d652266fb2e29e"],
+        owner: "6175b2ad05d652266fb2e29e"
     },
     {
         name: 'Hump Day Hot Shots',
         location: 'Coconut Beach',
         dateandtime: 'wednesday night. reg at 6 play at 7.',
         description: 'A relaxed volleyball tournament aimed at beginners so they can improve by playing all skill levels for cheap. good players are also enticed to come for cash prize. five dollar per player, max six players to a team. each team guranteed 4 games.',
-        entrants: 12,
+        entrants: ["6175b2ad05d652266fb2e29e", "6175b2ad05d652266fb2e29c"],
+        owner: "6175b2ad05d652266fb2e29c"
     },
     {
         name: 'Friday Night Hot Shots',
         location: 'White Sands / Seegers Sandbar',
         dateandtime: 'friday night, after leagues',
         description: 'standard hotshots, $5/player, 6player/team. late night. known to go as late as 2am.',
-        entrants: 24,
+        entrants: ["6175b2ad05d652266fb2e29e"],
+        owner: "6175b2ad05d652266fb2e29d"
     },
     {
         name: 'Emerald Coast Classic',
         location: 'okaloosa island',
         dateandtime: 'oct 12-17',
         description: 'a massive beach volleyball tournament. all the legends will come. everyone sandbags. good luck. $80 a person.',
-        entrants: 300,
+        entrants: ["6175b2ad05d652266fb2e29e", "6175b2ad05d652266fb2e29c", "6175b2ad05d652266fb2e29d"],
+        owner: "6175b2ad05d652266fb2e29d"
     },
     {
         name: 'Commodore Cup #4',
         location: 'Featheringill',
         dateandtime: 'saturday',
         description: 'the fourth event in the commodore cup circuit. must come to 3 to qualify for main event.',
-        entrants: 60,
+        entrants: ["6175b2ad05d652266fb2e29e"],
+        owner: "6175b2ad05d652266fb2e29e"
     }
 ]
 
