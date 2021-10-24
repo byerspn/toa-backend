a11428985813785c1cd6849bb9c87451bf5b3588
Parker Byers <parker.n.byers@gmail.com>
2021-10-24 18:28:09 -0500

    reverted to commit before last, and changed a route that may have caused app crash

--- controllers/events.js
+++ controllers/events.js
@@ -1,6 +1,6 @@
 const express = require('express')
 
-const { handleValidateId } = require('../middleware/custom_errors ')
+const { handleValidateId, handleRecordExists, } = require('../middleware/custom_errors')
 
 const router = express.Router()
 const Event = require('../models/Event')
@@ -23,12 +23,9 @@ router.get('/:id', handleValidateId, (req, res, next) => {
     Event.findById(req.params.id)
         .populate('entrants')
         .populate('owner')
-        .then(event => {
-            if (!event) {
-                res.sendStatus(404)
-            } else {
-                res.json(event)
-            }
+        .then(handleRecordExists)
+        .then((event) => {
+            res.json(event)
         })
         .catch(next)
 })
@@ -43,12 +40,9 @@ router.post('/', (req, res, next) => {
 // update one
 router.put('/:id', handleValidateId, (req, res, next) => {
     Event.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
-        .then(updatedEvent => {
-            if (!updatedEvent) {
-                res.sendStatus(404)
-            } else {
-                res.json(updatedEvent)
-            }
+        .then(handleRecordExists)
+        .then((updatedEvent) => {
+            res.json(updatedEvent)
         })
         .catch(next)
 })
@@ -56,16 +50,12 @@ router.put('/:id', handleValidateId, (req, res, next) => {
 // delete one
 router.delete('/:id', handleValidateId, (req, res, next) => {
     Event.findOneAndDelete({ _id: req.params.id })
-        .then(deletedEvent => {
-            if (!deletedEvent) {
-                res.sendStatus(404)
-            } else {
-                res.sendStatus(204)
-            }
+        .then(handleRecordExists)
+        .then((deletedEvent) => {
+            res.json(deletedEvent)
         })
         .catch(next)
 })
-
 
 
 module.exports = router
