5b8d9e62a93cd66759ebc7fcd96847cbb09ed55b
Parker Byers <parker.n.byers@gmail.com>
2021-10-24 12:36:04 -0500

    better error handling

--- controllers/events.js
+++ controllers/events.js
@@ -6,14 +6,26 @@ const Event = require('../models/Event')
 // show all. maybe add paginationlater
 router.get('/', (req, res, next) => {
     Event.find({})
-        .then(events => res.json(events))
+        .then(events => {
+            if (!events) {
+                res.sendStatus(404)
+            } else {
+                res.json(events)
+            }
+        })
         .catch(next)
 })
 
 // show one
 router.get('/:id', (req, res, next) => {
     Event.findById(req.params.id)
-        .then(event => res.json(event))
+        .then(event => {
+            if (!event) {
+                res.sendStatus(404)
+            } else {
+                res.json(event)
+            }
+        })
         .catch(next)
 })
 
@@ -27,14 +39,25 @@ router.post('/', (req, res, next) => {
 // update one
 router.put('/:id', (req, res, next) => {
     Event.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
-        .then(updatedEvent => res.json(updatedEvent))
+        .then(updatedEvent => {
+            if (!updatedEvent) {
+                res.sendStatus(404)
+            } else {
+                res.json(updatedEvent)
+            }
+        })
         .catch(next)
 })
 
 // delete one
 router.delete('/:id', (req, res, next) => {
     Event.findOneAndDelete({ _id: req.params.id })
-        .then(deletedEvent => res.json(deletedEvent))
+        .then(deletedEvent => {
+            if (!deletedEvent) {
+                res.sendStatus(404)
+            } else {
+                res.sendStatus(204)
+            }
         .catch(next)
 })
 
